6f4533a4719052c9b4e963b8bf6188538e63e9c4
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-26 21:29:44 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,100 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  computed,
+  effect,
+  inject,
+  input,
+  signal,
+  viewChild,
+  viewChildren,
+} from "@angular/core";
+import { Directive } from "@angular/core";
+
+@Directive({
+  selector: "[appSetColor]",
+  standalone: true,
+})
+export class SetColorDirective {
+  renderer = inject(Renderer2);
+  elementRef = inject(ElementRef);
+
+  colorInput = input.required<string>({ alias: "appSetColor" });
+
+  #color = signal("");
+
+  color = computed(() => this.#color());
+
+  constructor() {
+    effect(
+      () => {
+        this.setColor(this.colorInput());
+      },
+      { allowSignalWrites: true }
+    );
+
+    effect(() => {
+      this.renderer.setStyle(
+        this.elementRef.nativeElement,
+        "color",
+        this.#color()
+      );
+    });
+  }
+
+  setColor(color: string) {
+    this.#color.set(color);
+  }
+}
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SetColorDirective],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <div appSetColor="blue">Texto 1</div>
+    <div appSetColor="green">Texto 2</div>
+    <div appSetColor="purple">Texto 3</div>
+    <div appSetColor="red">Texto 4</div>
 
-    <router-outlet />
+    <hr>
+    
+    <p>Aplique preto em todas as diretivas!</p>
+    <button (click)="setAllColorsToBlack()">Aplicar</button>
+
+    <hr />
+
+    A cor da primeira diretiva é
+    <strong>{{ firstDirective().color() }}</strong> (recuperado com viewChild())
+
+    <hr />
+
+    Todas as cores de todas as diretivas (recuperadas com viewChildren())
+    <ul>
+      @for (item of directives(); track item.color(); let index = $index) {
+      <li>
+        A cor da {{ index + 1 }}° diretiva é <strong>{{ item.color() }}</strong>
+      </li>
+      }
+    </ul>
+
+    <hr />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'angular17-query-signals';
+export class AppComponent implements OnInit {
+  firstDirective = viewChild.required(SetColorDirective);
+  directives = viewChildren(SetColorDirective);
+
+  ngOnInit(): void {
+    // console.log(this.child()?.value)
+  }
+
+  setAllColorsToBlack() {
+    this.directives().forEach((setColorDirective) =>
+      setColorDirective.setColor("black")
+    );
+  }
 }
